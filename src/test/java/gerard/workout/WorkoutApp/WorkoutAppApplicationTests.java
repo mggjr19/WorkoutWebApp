e8610c3d718e57e35bc72eaff904795c0a30e4af
mggjr19 <mggjr19@gmail.com>
2026-01-08 15:22:30 -0500

    initial commit
    initial commit

---
+++ src/test/java/gerard/workout/WorkoutApp/WorkoutAppApplicationTests.java
@@ -0,0 +1,13 @@
+package gerard.workout.WorkoutApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
